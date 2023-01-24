a619bb73a57750492aeeb507b9b64c5399875217
ZeberMVP <rubeen722@gmail.com>
2023-01-24 18:34:53 +0100

    add/entire app

---
+++ utils/db_mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+//const DATABASE_URL = "mongodb://localhost:27017/fakeshop";
+mongoose.connect("mongodb://127.0.0.1:27017/fakeshop", { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+
+// Eventos
+db.on("error", error => console.log(error));
+db.once("open", () => console.log("connection to MongoDB established"));
+
+module.exports = mongoose;
